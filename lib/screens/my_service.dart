7293ed4a9d66c351e3c3ebbccd993082142e3bae
Mod <Mod@abd.com>
2019-07-23 10:30:14 +0700

    Create HeadDrawer

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -6,12 +6,48 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+  // Explicit
+
+  // Method
+  Widget showDrawerMenu() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          headMenu(),
+        ],
+      ),
+    );
+  }
+
+  Widget headMenu() {
+    return DrawerHeader(
+      decoration: BoxDecoration(),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 60.0,
+            height: 60.0,
+            child: Image.asset('images/logo.png'),
+          ),
+          Text(
+            'Mod',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 24.0,
+              )),Text('Login by')
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('My Service'),
-        ),
-        body: Text('body'));
+      appBar: AppBar(
+        title: Text('My Service'),
+      ),
+      body: Text('body'),
+      drawer: showDrawerMenu(),
+    );
   }
 }
