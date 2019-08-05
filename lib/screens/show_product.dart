bb90dcf0732913aeec674c251932e6a67c977143
Mod <Mod@abd.com>
2019-08-05 15:23:58 +0700

    Read Firestore success

--- lib/screens/show_product.dart
+++ lib/screens/show_product.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Showproduct extends StatefulWidget {
   @override
@@ -6,6 +9,36 @@ class Showproduct extends StatefulWidget {
 }
 
 class _ShowproductState extends State<Showproduct> {
+
+// Explicit
+Firestore firestore = Firestore.instance;
+StreamSubscription<QuerySnapshot> subscription;
+List<DocumentSnapshot> snapshots;
+
+//Method
+  @override
+  void initState(){
+    super. initState();
+    readFireStore();
+  }
+
+  Future<void> readFireStore()async{
+
+    CollectionReference collectionReference = firestore.collection('Product');
+    subscription = await collectionReference.snapshots().listen((dataSnapshot){
+      snapshots = dataSnapshot.documents;
+
+      for (var snapshot in snapshots) {
+        
+        String nameProduct = snapshot.data['Name'];
+        print('nameProduct = $nameProduct');
+      }
+
+    });
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
